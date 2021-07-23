dec9960b82ab6bfdd65dc6acc2bb0e85357c65f6
brahimjarallah <brahim.jarallah@gmail.com>
2021-07-23 12:30:50 +0100

    URL Shortener Microservice

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const app = express()
-const mongoose = require("mongoose")
-
+const dns = require("dns")
+const bodyParser = require("body-parser")
 // Basic Configuration
 const port = process.env.PORT || 3000
 
 app.use(cors())
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use("/public", express.static(`${process.cwd()}/public`))
 
 app.get("/", function (req, res) {
@@ -19,75 +20,44 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" })
 })
+const links = []
+let id = 0
+app.post("/api/shorturl/new", (req, res) => {
+  const { url } = req.body
+  const noHTTPSurl = url.replace(/^https?:\/\//, "")
+  dns.lookup(noHTTPSurl, (err) => {
+    if (err) {
+      return res.json({
+        error: "Invalid URL",
+      })
+    } else {
+      id++
+      const link = {
+        original_url: url,
+        short_url: `${id}`,
+      }
+      links.push(link)
+      console.log(links)
+      return res.json(link)
+    }
+  })
+})
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const { id } = req.params
+  console.log(id, "<===== id query")
+  const link = links.find((l) => l.short_url === id)
+  console.log(link, "<===== link found")
+
+  if (link) {
+    return res.redirect(link.original_url)
+  } else {
+    return res.json({
+      error: "No short url",
+    })
+  }
+})
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`)
-})
-
-/* Database Connection */
-let uri =
-  "mongodb+srv://brahim:" +
-  process.env.PW +
-  "@cluster0.nab8p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-mongoose.connect(uri, { userNewUrlParser: true, useUnifiedTopology: true })
-
-/*Creating the url model */
-let urlSchema = new mongoose.Schema({
-  orginal: { type: String, required: true },
-  short: Number,
-})
-
-let Url = mongoose.model("Url", urlSchema)
-
-/* getting the url parameter*/
-let bodyParser = require("body-parser")
-
-let responseObject = {}
-app.post(
-  "/api/shorturl/new",
-  bodyParser.urlencoded({ extended: false }),
-  (req, res) => {
-    let inputUrl = req.body["url"]
-    responseObject["original_url"] = inputUrl
-    /*pass url as parameter and then receive json shortened url */
-    let inputShort = 1
-
-    Url.findOne({})
-      .sort({ short: "desc" })
-      .exec((error, result) => {
-        if (!error && result != undefined) {
-          inputShort = result.short + 1
-        }
-        if (!error) {
-          Url.findOneAndUpdate(
-            { original: inputUrl },
-            { original: inputUrl, short: inputShort },
-            { new: true, upsert: true },
-            (error, savedUrl) => {
-              if (!error) {
-                responseObject["short_url"] = savedUrl.short
-                res.json(responseObject)
-              }
-            }
-          )
-        }
-      })
-  }
-)
-
-//     res.json(responseObject)
-//   }
-// )
-
-
-app.get("/api/shorturl/:input", (request, response) => {
-  let input = request.params.input
-
-  Url.findOne({ short: input }, (error, result) => {
-    if (!error && result != undefined) {
-      response.redirect(result.original)
-    } else {
-      response.json("URL not Found")
-    }
-  })
 })
